f9427c5daac2dec70a5b5e6683c0978724d7317e
AjayKamath10 <84276017+AjayKamath10@users.noreply.github.com>
2024-01-25 16:10:39 +0530

    Add files via upload

---
+++ ScannerProject/Scanner.cs
@@ -0,0 +1,10 @@
+﻿namespace ScannerProject
+{
+    public class Scanner : IScanner
+    {
+        public void Scan(string path)
+        {
+            System.Console.WriteLine($"Scanning .....{path}");
+        }
+    }
+}
---
+++ TaskManagerProject/TaskManager.cs
@@ -0,0 +1,30 @@
+﻿using PrinterProject;
+using ScannerProject;
+using PrintScannerProject;
+
+namespace TaskManagerProject
+
+{
+    public static class TaskManager
+    {
+        
+        public static void ExecutePrintTask(IPrinter printer, string documentPath)
+        {
+            printer.Print(documentPath);
+        }
+        public static void ExecuteScanTask(IScanner scanner, string documentPath)
+        {
+            scanner.Scan(documentPath);
+        }
+        public static void ExecutePrintTask(IPrintScanner printScanner, string documentPath)
+        {
+
+            printScanner.ExecutePrintTask(documentPath);
+        }
+
+        public static void ExecuteScanTask(IPrintScanner printScanner, string documentPath)
+        {
+            printScanner.ExecuteScanTask(documentPath);
+        }
+    }
+}
