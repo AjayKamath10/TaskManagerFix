3d1a51684ebb7a70a0b443f8a8ed3de774b15445
Ajay Kamath <ajayvkamath10@gmail.com>
2024-01-25 15:44:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using PrinterProject;
+using ScannerProject;
+using TaskManagerProject;
+using PrintScannerProject;
+
+namespace TaskManagerAssignment
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Printer printerObj = new Printer();
+            Scanner scannerObj = new Scanner();
+            PrintScanner printScannerObj = new PrintScanner();
+
+            TaskManager.ExecutePrintTask(printerObj, "Test.doc");
+            TaskManager.ExecuteScanTask(scannerObj, "MyImage.png");
+            TaskManager.ExecutePrintTask(printScannerObj, "NewDoc.doc");
+            TaskManager.ExecuteScanTask(printScannerObj, "YourImage.png");
+            Console.Read();
+        }
+    }
+}
